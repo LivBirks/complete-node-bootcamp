36f1121d648894ef54da8f230dcf202d1ec0dfe1
Liv Birks <92850547+LivBirks@users.noreply.github.com>
2025-09-14 11:38:56 +0100

    creating web server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+
+/////////////////////////////////////////////////////////////////////////
+// FILES
 
 // Read Files
 // const textInput = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
@@ -9,21 +13,35 @@ const fs = require('fs');
 // console.log("File written.")
 
 //Async
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
-            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('your file has been written ✍️');
-            });
-        });
-    });
-});
-console.log('will read file...');
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('your file has been written ✍️');
+//             });
+//         });
+//     });
+// });
+// console.log('will read file...');
 
 // error handling
-fs.readFile('abc.txt', 'utf-8', (err, data) => {
-    if (err) return console.log('Error 🛑');
-    console.log('success 🟢');
+// fs.readFile('abc.txt', 'utf-8', (err, data) => {
+//     if (err) return console.log('Error 🛑');
+//     console.log('success 🟢');
+// });
+
+/////////////////////////////////////////////////////////////////////////
+// SERVER
+
+// accepts a callbackfunction, request and reponse variables
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server');
+});
+
+//takes port, local host address, optional - callbackfunction to run when it's set up
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listening to requests on port 8000')
 });
